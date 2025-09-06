c444b4c2d7f4e89ffbfd335e5fe7675dcee6e85a
Gohankr45444 <gohankr45444@gmail.com>
2025-09-06 21:46:37 +0000

    hello

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BirthdayWish from "./BirthdayWish";
-
-function App() {
-  return <BirthdayWish />;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import BirthdayWish from "./components/BirthdayWish";
+
+export default function App() {
+  return <BirthdayWish />;
+}
